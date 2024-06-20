5eb2f7f03f3c54056808ac5816de50acf1492e50
hayato94087 <hayato94087@gmail.com>
2024-06-20 15:11:00 +0900

    Server Actions の追加

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,18 +1,14 @@
 import { auth } from "@/auth";
+import WhoAmIServerAction from "./WhoAmIServerAction";
 
 export default async function TestRoute() {
   const session = await auth();
-
-  async function onGetUserAction() {
-    "use server";
-    const session = await auth();
-    return session?.user?.name ?? null;
-  }
 
   return (
     <main>
       <h1 className="text-3xl mb-5">Test Route</h1>
       <div>User: {session?.user?.name}</div>
+      <WhoAmIServerAction />
     </main>
   );
 }
